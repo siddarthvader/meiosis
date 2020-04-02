4acc6f80438e0389bce4f2bd385147d8b8642f4d
siddharth <siddhartjn93@gmail.com>
2020-04-02 13:32:46 +0530

    meiosis playground

--- src/app.js
+++ src/app.js
@@ -1,5 +1,36 @@
-var root = document.body;
-var count = 0;
+const merge = mergerino;
+var root = document.getElementById("root");
+
+const temperature = {
+  initial: {
+    value: 22,
+    units: "C"
+  },
+  Actions() {
+    return {
+      increment: function(amount) {
+        update(function(state) {
+          state.value += amount;
+          return state;
+        });
+      }
+    };
+  }
+};
+
+var update = flyd.stream();
+
+var states = flyd.scan(
+  function(state, patch) {
+    return patch(state);
+  },
+  temperature.initial,
+  update
+);
+var actions = temperature.Actions(update);
+states.map(function(state) {
+  console.log("<pre>" + JSON.stringify(state) + "</pre>");
+});
 
 var Hello = {
   view: function() {
@@ -9,16 +40,14 @@ var Hello = {
         {
           class: "title"
         },
-        "My first app"
+        "My first app in culture"
       ),
       m(
         "button",
         {
-          onclick: function() {
-            count++;
-          }
+          onclick: actions.increment
         },
-        count + " clicks"
+        temperature.initial.value + " clicks"
       )
     ]);
   }
@@ -40,3 +69,7 @@ m.route(root, "/splash", {
   "/splash": Splash,
   "/hello": Hello
 });
+
+// Only for using Meiosis Tracer in development.
+meiosis.trace({ update, dataStreams: [states] });
+meiosisTracer({ selector: "#tracer" });
